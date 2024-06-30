4b071098d01a3c492c5527d2a145cf587228f59c
Kesavan Yogeswaran <kesyog@gmail.com>
2024-06-30 12:59:53 -0400

    nvim: Add more nvim-cmp sources
    Add sources for emojis and paths

--- nvim/lua/plugins/main.lua
+++ nvim/lua/plugins/main.lua
@@ -112,11 +112,12 @@ return {
   },
   {
     'hrsh7th/nvim-cmp',
-    lazy = false,
     dependencies = { 
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-nvim-lsp-signature-help',
       'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-emoji',
+      'hrsh7th/cmp-path',
     },
     config = function()
      local cmp = require('cmp')
@@ -125,12 +126,29 @@ return {
           { name = 'nvim_lsp' },
           { name = 'nvim_lsp_signature_help' },
           { name = 'buffer' },
+          { name = 'emoji' },
+          { name = 'path' },
         }),
         mapping = cmp.mapping.preset.insert({
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete(),
+          ['<tab>'] = cmp.mapping(function(original)
+            if cmp.visible() then
+              cmp.select_next_item()
+            else
+              original()
+            end
+          end, {'i', 's'}),
+          ['<S-tab>'] = cmp.mapping(function(original)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            else
+              original()
+            end
+          end, {'i', 's'}),
           ['<C-e>'] = cmp.mapping.abort(),
+          ['<Esc>'] = cmp.mapping.abort(),
           ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         }),
       }, {

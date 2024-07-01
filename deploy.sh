d8f8ce5f2a2bf7197209ad4d630b8dec0fd0d0ea
Kesavan Yogeswaran <hikes@google.com>
2024-07-01 14:25:41 +0000

    Remove obsolete fzf config

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,16 @@
 #!/bin/sh
 
 # deploy dotfiles with stow
-#./stowsh/stowsh alacritty -t ~/.config/alacritty
-# ./stowsh/stowsh bash -t ~
-# ./stowsh/stowsh fzf -t ~
+# ./stowsh/stowsh alacritty -t ~/.config/alacritty
 # ./stowsh/stowsh gdb -t ~
-./stowsh/stowsh git -t ~
-./stowsh/stowsh i3 -t ~/.config/i3
-./stowsh/stowsh nvim -t ~/.config/nvim
-./stowsh/stowsh nvim/lua -t ~/.config/nvim/lua
-./stowsh/stowsh nvim/lua/config -t ~/.config/nvim/lua/config
-./stowsh/stowsh nvim/lua/plugins -t ~/.config/nvim/lua/plugins
+# ./stowsh/stowsh git -t ~
+# ./stowsh/stowsh i3 -t ~/.config/i3
+# ./stowsh/stowsh nvim -t ~/.config/nvim
+# ./stowsh/stowsh nvim/lua -t ~/.config/nvim/lua
+# ./stowsh/stowsh nvim/lua/config -t ~/.config/nvim/lua/config
+# ./stowsh/stowsh nvim/lua/plugins -t ~/.config/nvim/lua/plugins
 # ./stowsh/stowsh scm_breeze -t ~
-./stowsh/stowsh starship -t ~/.config
-./stowsh/stowsh tmux -t ~
-# ./stowsh/stowsh vim -t ~
+# ./stowsh/stowsh starship -t ~/.config
+# ./stowsh/stowsh tmux -t ~
 # ./stowsh/stowsh vscode -t ~/.config/Code/User
-./stowsh/stowsh zsh -t ~
-
+# ./stowsh/stowsh zsh -t ~

d7fba52878d2045001117a8207ee886eae05cbeb
Kesavan Yogeswaran <hikes@google.com>
2024-08-19 15:05:18 +0000

    Comment out query-driver arg from clangd command

--- nvim/lua/plugin_setup.lua
+++ nvim/lua/plugin_setup.lua
@@ -3,13 +3,14 @@
 --- Must be run after nvim_cmp and neoconf setup
 local function lspconfig_setup()
   local cmd
-  local root_dir
+  local root_dir = nil
+  --- TODO: do better
   if os.getenv('PW_PROJECT_ROOT') == nil then
     cmd = {'clangd', '--clang-tidy'}
   else
     cmd = {'clangd',
     '--compile-commands-dir=' .. os.getenv('PW_PROJECT_ROOT') .. '/.pw_ide/.stable',
-    '--query-driver=' .. os.getenv('PW_WAC_CIPD_INSTALL_DIR') .. '/bin/*,' .. os.getenv('PW_PIGWEED_CIPD_INSTALL_DIR') .. '/bin/*',
+    --'--query-driver=' .. os.getenv('PW_WAC_CIPD_INSTALL_DIR') .. '/bin/*,' .. os.getenv('PW_PIGWEED_CIPD_INSTALL_DIR') .. '/bin/*',
     '--background-index',
     '--clang-tidy',
     }
